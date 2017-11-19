5f88edff7daa99a3df1a0e47ef26f7a3d3698a84
İlknur Meray <lknur Meray>
2017-11-19 22:17:52 +0300

    rsa encryption/decryption test - does not convert to image

--- sigsite/views.py
+++ sigsite/views.py
@@ -3,10 +3,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+from Crypto.PublicKey import RSA
+from Crypto import Random
 import base64
 import numpy as np
+import ast
 
 class TestView(View):
+    def generate_key_pair(self):
+        rand_num_generator = Random.new().read
+        pb_pr_key = RSA.generate(1024, rand_num_generator) # public-private key pair generation
+        return pb_pr_key
 
     def get(self, request, *args, **kwargs):
         return render(request, 'index2.html', {})
@@ -18,8 +25,50 @@ class TestView(View):
         b64_data = request.POST['hidden']
         cleared_data = b64_data[22:]
         print(len(cleared_data) % 4)
+
+        pb_pr_key = self.generate_key_pair()
+        pb_key = pb_pr_key.publickey() # separate public key to use in encryption
+        print(pb_key)
+        # Image encryption/decryption
+        encrypted_data = pb_key.encrypt(base64.b64decode(cleared_data), 32)
+        print(encrypted_data)
+        fh = open("imageToSave.txt", "w")
+        fh.write(str(encrypted_data))
+        fh.close()
+
+        f = open ("imageToSave.txt", 'r')
+        data = f.read()
+        print(data)
+        decrypted_data = pb_pr_key.decrypt(ast.literal_eval(str(data)))
+        print("------------------------------------------------")
+        print(decrypted_data)
+        print("------------------------------------------------")
+        print(base64.b64encode(decrypted_data))
+        print("------------------------------------------------")
+        fh = open("imageToSave.png", "wb")
+        fh.write(decrypted_data)
+        fh.close()
+        '''
+        # Message encryption/decryption
+        encrypted_data = pb_key.encrypt(base64.b64encode(b'hello'), 32)
+        print(encrypted_data)
+        fh = open("imageToSave.txt", "w")
+        fh.write(str(encrypted_data))
+        fh.close()
+
+        f = open ("imageToSave.txt", 'r')
+        data = f.read()
+        print(data)
+        decrypted_data = pb_pr_key.decrypt(ast.literal_eval(str(data)))
+        print("------------------------------------------------")
+        print(decrypted_data)
+        print("------------------------------------------------")
+        print(base64.b64decode(decrypted_data))
+        '''
+        '''
         # Save file to system just for checking
         fh = open("imageToSave.png", "wb")
         fh.write(base64.b64decode(cleared_data))
         fh.close()
+        '''
         return render(request, 'index2.html', {})
