2247206098b7e8441734bb00cc3c650e4c38e6a9
HalitU <uyanikhalit1453@gmail.com>
2017-11-20 08:25:38 +0300

    Views.py finalized encode and decode process

--- sigsite/views.py
+++ sigsite/views.py
@@ -19,56 +19,47 @@ class TestView(View):
         return render(request, 'index2.html', {})
 
     def post(self, request, *args, **kwargs):
-        print("Kek")
-        print(request.POST['haha'])
-        print(request.POST['hidden'])
+
+        # Getting the image data
         b64_data = request.POST['hidden']
+        # Splitting the initial unnecessary data
         cleared_data = b64_data[22:]
-        
+        # Data needs to be a multiply of 4
         print(len(cleared_data) % 4)
+        # Generate keys
         pb_pr_key = self.generate_key_pair()
         pb_key = pb_pr_key.publickey() # separate public key to use in encryption
         print(pb_key)
-        # Image encryption/decryption
-        encrypted_data = pb_key.encrypt(base64.b64decode(cleared_data), 32)
-        print(encrypted_data)
-        fh = open("imageToSave.txt", "w")
-        fh.write(str(encrypted_data))
-        fh.close()
 
-        f = open ("imageToSave.txt", 'r')
-        data = f.read()
-        print(data)
-        decrypted_data = pb_pr_key.decrypt(ast.literal_eval(str(data)))
-        print("------------------------------------------------")
-        print(decrypted_data)
-        print("------------------------------------------------")
-        print(base64.b64encode(decrypted_data))
-        print("------------------------------------------------")
-        fh = open("imageToSave.png", "wb")
-        fh.write(decrypted_data)
-        fh.close()
-        '''
-        # Message encryption/decryption
-        encrypted_data = pb_key.encrypt(base64.b64encode(b'hello'), 32)
-        print(encrypted_data)
+        # ENCRYPTION PART
+        decoded_data = base64.b64decode(cleared_data)
+        # Split data into 128 character parts
+        splitted_cleared_data = [cleared_data[i:i+128] for i in range(0, len(cleared_data), 128)]
+        # Encrypt all the data in array
+        splitted_encrypt_data = [pb_key.encrypt(str(i), 32) for i in splitted_cleared_data]
+        # Write encrypted array to file
         fh = open("imageToSave.txt", "w")
-        fh.write(str(encrypted_data))
+        for item in splitted_encrypt_data:
+            fh.write("%s \n" % str(item))
         fh.close()
-
-        f = open ("imageToSave.txt", 'r')
-        data = f.read()
-        print(data)
-        decrypted_data = pb_pr_key.decrypt(ast.literal_eval(str(data)))
-        print("------------------------------------------------")
-        print(decrypted_data)
-        print("------------------------------------------------")
-        print(base64.b64decode(decrypted_data))
-        '''
-        '''
-        # Save file to system just for checking
+        # If you want to see the image file
         fh = open("imageToSave.png", "wb")
         fh.write(base64.b64decode(cleared_data))
         fh.close()
-        '''
+        # DECODING PART
+        # Read crypted data from the file
+        with open("imageToSave.txt") as f:
+            content = f.readlines()
+        # Divide it into the array
+        content = [x.strip() for x in content]
+        # Decode the data
+        splitted_decrypt_data = [pb_pr_key.decrypt(ast.literal_eval(str(i))) for i in content]
+
+        # Checking initial and encoded data to be equal
+        is_equal = True
+        for i in range(0, len(splitted_cleared_data)):
+            if str(splitted_cleared_data[i]) != splitted_decrypt_data[i]:
+                is_equal = False
+        print is_equal 
+
         return render(request, 'index2.html', {})
